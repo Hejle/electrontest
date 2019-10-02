0b052af98585f8d5356ecef19b2366a8117c13fa
Joachim Hejlesen <johe@hesehus.dk>
2019-10-02 12:10:07 +0200

    First commit

---
+++ addon.cpp
@@ -0,0 +1,24 @@
+#include <nan.h>
+using namespace std;
+using namespace Nan;
+using namespace v8;
+
+// Accepts 1 number from JavaScript, adds 42 and returns the result.
+NAN_METHOD(PassNumber)
+{
+    Nan::Maybe<double> value = Nan::To<double>(info[0]);
+    Local<Number> retval = Nan::New(value.FromJust() + 42);
+    info.GetReturnValue().Set(retval);
+}
+
+// Called by the NODE_MODULE macro below,
+// exposes a pass_number method to JavaScript, which maps to PassNumber
+// above.
+NAN_MODULE_INIT(Init)
+{
+    Nan::Set(target, New<String>("pass_number").ToLocalChecked(),
+             GetFunction(New<FunctionTemplate>(PassNumber)).ToLocalChecked());
+}
+
+// macro to load the module when require'd
+NODE_MODULE(addon, Init)
